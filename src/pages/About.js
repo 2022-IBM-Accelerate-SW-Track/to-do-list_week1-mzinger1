aee605447ec4975d72da5ff55e987d35c65ce5b7
mzinger1 <michellezinger23@gmail.com>
2022-06-08 20:03:02 -0400

    initial commit

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
-  
+import profile_image from '../../src/MichelleZinger.jpg';
+
 export default class About extends Component {
   render() {
     return (
       <div>
-        <p>Design your About me page </p> 
+        <h1>About me: </h1> 
+        <li>
+        <p> I am a rising Junior at Northeastern University in Boston, MA. </p>
+        <p> I am studying Computer Science with a concentration in Software. </p>
+        <p> I am originally from Westchester, NY.</p>
+        </li>
+        <img src={profile_image} alt="Profile" width = "270" height = "400"/>;
       </div>
     )
   }
